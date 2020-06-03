f52b3464039d7d56d90f353fb2a5c3b19cb78e7c
nitrag <nitrag@users.noreply.github.com>
2020-06-02 23:31:07 -0400

    more granular tracks

--- layers/transportation/layer.sql
+++ layers/transportation/layer.sql
@@ -166,12 +166,12 @@ indoor INT, bicycle TEXT, foot TEXT, horse TEXT, mtb_scale TEXT, surface TEXT) A
         FROM osm_highway_linestring
         WHERE NOT is_area AND (
             zoom_level = 12 AND (
-                highway_class(highway, public_transport, construction) NOT IN ('track', 'path', 'minor')
+                highway_class(highway, public_transport, construction) NOT IN ('minor')
                 OR highway IN ('unclassified', 'residential')
             ) AND man_made <> 'pier'
             OR zoom_level = 13
                 AND (
-                    highway_class(highway, public_transport, construction) NOT IN ('track', 'path') AND man_made <> 'pier'
+                    man_made <> 'pier'
                 OR
                     man_made = 'pier' AND NOT ST_IsClosed(geometry)
                 )
